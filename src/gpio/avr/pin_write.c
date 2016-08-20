178b573e98782080e03c462179ca18a4fb2c6346
Chad Fraleigh <chadf@triularity.org>
2016-08-20 22:23:18 +0000

    Moved timer stop after pin set to avoid transition gaps.

--- src/gpio/avr/pin_write.c
+++ src/gpio/avr/pin_write.c
@@ -18,28 +18,27 @@ gpio_pin_write
 )
 {
 	gpio_iooff_t		offset;
-	const gpio_timer_t *	timer;
 	gpio_ioptr8_t		port;
 	uint8_t			bitmask;
+	const gpio_timer_t *	timer;
 
 
 //_gpio_pin_dump(pin);
 	if((offset = PGM_IOOFF(pin->port)) != GPIO_NO_REGISTER)
 	{
 		port = IOOFF_TO_PTR8(offset);
-
-		/*
-		 * Turn off the PWM timer (in case it is running)
-		 */
-		if((timer = PGM_PTR(pin->timer)) != GPIO_NO_TIMER)
-			_gpio_pwm_stop(timer);
-
 		bitmask = PGM_BYTE(pin->bitmask);
 
 		if(state)
 			*port |= bitmask;
 		else
 			*port &= ~bitmask;
+
+		/*
+		 * Turn off the PWM timer (in case it is running)
+		 */
+		if((timer = PGM_PTR(pin->timer)) != GPIO_NO_TIMER)
+			_gpio_pwm_stop(timer);
 	}
 	else
 	{
